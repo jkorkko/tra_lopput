226149d825cc0f9f3fea7dd8f783bb86799fe300
Jeremias Körkkö <jeremias.korkko@hotmail.com>
2021-03-15 10:27:46 +0200

    Työselostus + päivitetty kommentoitu versio lopputyöstä.

--- src/tra_loppytyo.py
+++ src/tra_loppytyo.py
@@ -1,30 +1,36 @@
 def ask_file():
-        print("\nType the name of the input file. For example \"test.txt\".")
-        while(True):
-            try:
-                print("")
-                input_file = input("Name of the file: ")
-                if input_file[-4:] == ".txt":
-                    #Get rid of imported lib trough FileNotFoundError?
-                    with open(input_file) as test:
-                        pass
-                    return input_file
-                else:
-                    print("Expecing a .txt file.")
-                    continue
-            except FileNotFoundError:
-                print("No", input_file, "in directory.")
+    '''
+    Kysy käyttäjältä syötetiedostoa
+    '''
+    print("\n Anna syötetiedoston nimi. Esimerkiksi: \"test.txt\".")
+    while(True):
+        try:
+            print("")
+            input_file = input(" Tiedoston nimi: ")
+            if input_file[-4:] == ".txt":
+                # Varmista, että tiedosto löytyy kansiosta
+                with open(input_file) as test:
+                    pass
+                return input_file
+            else:
+                print(" Odotetaan \".txt\"-tiedostoa.")
                 continue
+        except FileNotFoundError:
+            print(" Tiedostoa", input_file, "ei löydy kansiosta.")
+            continue
 
 
-def read_file(data, graph):
+def read_file(data):
     '''
-    Open & read info from the given input file.
+    Lue graafi syötteestä.
     '''
+    graph = []
     with open(data) as input_file:
         input_file = input_file.readlines()
         vertice, edge = input_file.pop(0).split()
         DESTINATION = int(input_file.pop(-1))
+        # Luo lista kaikista graafin sivuista
+        # nousevassa järjetyksessä.
         for i in input_file:
             i = i.split()
             i = tuple(map(int, i))
@@ -35,7 +41,9 @@ def read_file(data, graph):
 
 def find(cycle, start):
     '''
-    This function is made by Neelam Yadav
+    Etsi vanhempi.
+
+    -Tämän funktion on tehnyt Neelam Yadav-
     https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
     '''
     if cycle[start] == start:
@@ -45,15 +53,16 @@ def find(cycle, start):
 
 def create_mst(graph, VERTICES):
     '''
-    Create minimum spanning tree.
+    Muodosta minimivirittäväpuu Kruskalin algoritmilla.
 
-    This function is based on code made by Neelam Yadav
+    -Osa funktiosta perustuu Neelam Yadavin tekemään koodiin-
     https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
     '''
-    mst = []    #minimum spanninfg tree representation
-    cycle = []  #prevent creating cycles in mst
-    level = []  #nodes attached to each other
+    mst = []    # minimivirittävä puu
+    cycle = []
+    level = []
     j, found = 0, 0
+    # Alusta listat oikeilla arvoilla
     for i in range(VERTICES):
         cycle.append(i)
         level.append(0)
@@ -61,12 +70,16 @@ def create_mst(graph, VERTICES):
     while found < VERTICES - 1:
         start, end, height = graph[j]
         j += 1
+        # Etsi vanhemmat käyttäen Path Compressionia
         start_index = find(cycle, start)
         end_index = find(cycle, end)
+        # -Jos tosi, sivu ei luo piiriä, lisätään mst:hen.
+        # -Jos epätosi, hylkää sivu.
         if start_index != end_index:
             mst[start].append((end, height))
             mst[end].append((start, height))
             found += 1
+            # Path Compression
             if level[start_index] > level[end_index]:
                 cycle[end_index] = start_index
             elif level[start_index] < level[end_index]:
@@ -79,37 +92,36 @@ def create_mst(graph, VERTICES):
 
 def find_path(mst, DESTINATION):
     '''
-    Find path from point 1 to destination from
-    minimum spanning tree
+    Etsi reitti kaupungista 1 määränpäähän
+    minimivirittävästä puusta käyttäen syvyyshakua.
     '''
     path, heights = [0], []
     while path[-1] != DESTINATION:
         if len(mst[path[-1]]) == 0:
+            # Polku päättyi umpikujaan, palataan takaisin päin
             path.pop(-1)
             heights.pop(-1)
             continue
         else:
+            # Uusi sivu löydettiin
             new_vertice, new_height = mst[path[-1]][0]
             path.append(new_vertice)
             heights.append(new_height)
+        # Poista tutkitut sivut
         mst[new_vertice].remove((path[-2], new_height))
         mst[path[-2]].pop(0)
     for v in path:
-        print(v + 1)
+        print("", v + 1)
     return max(heights)
 
 
 def main():
-    '''
-    Run algorithm
-    '''
-    graph = []  #full map representation
     input_file = ask_file()
-    VERTICES, DESTINATION, graph = read_file(input_file, graph)
+    VERTICES, DESTINATION, graph = read_file(input_file)
     mst = create_mst(graph, int(VERTICES))
-    print("reitti:")
+    print(" reitti:")
     answer = find_path(mst, DESTINATION - 1)
-    print("matalimman reitin korkein kohta", answer)
+    print(" vastaus:", answer)
 
 
 main()
